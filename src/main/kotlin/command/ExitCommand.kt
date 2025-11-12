1aae8bf6b03e8d1c2a961a14f7dca5a5c904f62e
EveryPine <kenton.lim@gmail.com>
2025-11-12 17:03:05 +0900

    fix(exit-command): 오타 수정

--- src/main/kotlin/command/ExitCommand.kt
+++ src/main/kotlin/command/ExitCommand.kt
@@ -11,7 +11,7 @@ class ExitCommand(private val gameManager: GameManager): Command {
 
     private fun validate(args: List<String>) {
         if (args.isNotEmpty()) {
-            throw IllegalArgumentException("status 명령은 인자를 받을 수 없습니다")
+            throw IllegalArgumentException("exit 명령은 인자를 받을 수 없습니다")
         }
     }
 }
